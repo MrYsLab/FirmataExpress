10bc0dc64742015ac1f5b9f73415d258571c82ef
MrYsLab <MisterYsLab@gmail.com>
2021-05-26 13:58:58 -0400

    DHT Fixes

--- FirmataConstants.h
+++ FirmataConstants.h
@@ -32,8 +32,8 @@ namespace firmata {
  * Query using the REPORT_FIRMWARE message.
  */
 static const int FIRMWARE_MAJOR_VERSION =  1;
-static const int FIRMWARE_MINOR_VERSION =  1;
-static const int FIRMWARE_BUGFIX_VERSION = 5;
+static const int FIRMWARE_MINOR_VERSION =  2;
+static const int FIRMWARE_BUGFIX_VERSION = 0;
 
 /* Version numbers for the protocol.  The protocol is still changing, so these
  * version numbers are important.
